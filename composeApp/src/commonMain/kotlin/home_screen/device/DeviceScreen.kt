261a9d91682f2dce68d79c8cb691cc65babea49c
AppTrailblazer <rabit.cr.257@gmail.com>
2024-03-28 01:10:44 -0500

    update

--- composeApp/src/commonMain/kotlin/home_screen/device/DeviceScreen.kt
+++ composeApp/src/commonMain/kotlin/home_screen/device/DeviceScreen.kt
@@ -46,7 +46,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 data class DeviceScreen(
     val selectedService: MutableStateFlow<List<InnerImage?>> = MutableStateFlow(listOf()),
     val selectedPhoneIcon: MutableStateFlow<List<InnerInfo?>> = MutableStateFlow(listOf())
-
 ) : Screen {
 
     @Composable
